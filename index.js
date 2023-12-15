65c1cdd940b9015a9cfb8a316d2187d60ed60bf9
Keeley Madigan <keeleysprouts@gmail.com>
2023-12-15 00:13:20 -0800

    js looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const Cards = [];
+function writeCards(name){
+  for (let i = 0; i < name.length; i++){
+    Cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+};
+  ;
+ return Cards
+};
+
+
+var k = "10";
+function countDown(k)
+{
+while (k >= 0) 
+{
+  console.log(k);
+  k--;
+}
+}
+countDown();
